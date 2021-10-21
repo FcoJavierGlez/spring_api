fafccf5587a46ffdb6abd94223f4ffff5e21f9a3
FcoJavierGlez <FJGS_86@hotmail.com>
2021-10-21 08:03:41 +0200

    Actualizado

--- src/main/java/com/nttdata/bootcamp/api/controller/Controller.java
+++ src/main/java/com/nttdata/bootcamp/api/controller/Controller.java
@@ -1,25 +1,19 @@
-package com.nttdata.api.controller;
-
-import javax.websocket.server.PathParam;
+package com.nttdata.bootcamp.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nttdata.bootcamp.library.config.AdultProperties;
 
 @RestController
-@RequestMapping("/api/")
 public class Controller {
 	
 	@Autowired
 	AdultProperties adultProperties;
 	
 	@GetMapping("/age")
-	@ResponseBody
 	public int getAge() {
 		System.out.println( "Llamada a adultProperties" + adultProperties.getAge() );
 		return adultProperties.getAge();
